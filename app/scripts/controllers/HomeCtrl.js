a51499c787ada08bfefc3c0916b6f55b7678303f
Javier Forero <javierforero01@gmail.com>
2016-09-11 09:29:49 -0500

    inject not working

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,9 @@
 (function(){
-  function HomeCtrl() {
-    this.pana = "fuck you"
+  function HomeCtrl(Room) {
+    this.testPhrase = 'Hello, this is my test phrase in the home controller';
   }
 
   angular
     .module('blocChat')
-    .controller('HomeCtrl', HomeCtrl);
+    .controller('HomeCtrl', [Room', HomeCtrl]);
 })();
--- dist/scripts/services/FireBase.js
+++ dist/scripts/services/FireBase.js
@@ -1,8 +1,6 @@
-(function(){
-
+(function() {
   function Room($firebaseArray) {
-
-    var ref = firebase.database().ref();
+    var ref = firebase.database().ref().child("rooms");
     var rooms = $firebaseArray(ref);
 
     return {
