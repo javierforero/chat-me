dc9797b1182f657893cfe0d4e13b0fd1db2f02a1
Javier Forero <javierforero01@gmail.com>
2016-09-14 07:31:17 -0500

    checkpoint-3-completed

--- app/scripts/controllers/AddWindowCtrl.js
+++ app/scripts/controllers/AddWindowCtrl.js
@@ -1,11 +1,24 @@
 (function() {
-  function AddWindowCtrl($uibModalInstance) {
+  function AddWindowCtrl($uibModalInstance, Room) {
+
+    this.roomService = Room;
+
     this.dismiss = function() {
       $uibModalInstance.dismiss('cancel');
+    };
+
+    this.submit = function() {
+      if(this.text) {
+
+        this.roomService.addRoom(this.text);
+        this.text = '';
+        $uibModalInstance.close();
+      }
+
     };
   }
 
   angular
     .module('blocChat')
-    .controller('AddWindowCtrl', ['$uibModalInstance', AddWindowCtrl]);
+    .controller('AddWindowCtrl', ['$uibModalInstance','Room', AddWindowCtrl]);
 })();
