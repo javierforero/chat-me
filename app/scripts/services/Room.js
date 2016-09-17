858b47ac7f8093be5af3fd1e562b51d661848d3d
Javier Forero <javierforero01@gmail.com>
2016-09-17 15:11:40 -0500

    having loop issues

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -1,20 +1,47 @@
 (function() {
   function Room($firebaseArray) {
-    var ref = firebase.database().ref().child("rooms");
-    var rooms = $firebaseArray(ref);
+
+    var roomsRef = firebase.database().ref().child("rooms");
+    var messagesRef = firebase.database().ref().child("messages");
+    var rooms = $firebaseArray(roomsRef);
+    var messages = $firebaseArray(messagesRef);
     var roomService = {};
 
-    
+
     roomService.all = rooms;
-    roomService.addRoom = function (roomTitle) {
-      rooms.$add({name: roomTitle});
-    };
-
     roomService.currentRoom = null;
+    roomService.messages = null;
 
-    roomService.setSong = function(song) {
-      roomService.currentRoom = song;
+
+    roomService.addRoom = function (roomTitle) {
+      rooms.$add({
+        name: roomTitle,
+        created_at: firebase.database.ServerValue.TIMESTAMP
+      });
     };
+
+    roomService.addMessage = function(userName, content, roomId) {
+
+      messages.$add({
+        username: userName,
+        content: content,
+        sent_at: firebase.database.ServerValue.TIMESTAMP,
+        roomid: roomId
+      });
+    };
+
+    roomService.getMessages = function (roomId) {
+
+      if(roomService.currentRoom == null) {
+
+        return $firebaseArray(messagesRef.orderByChild('roomid').equalTo(roomId));
+      }
+    };
+
+    roomService.setRoom = function(room) {
+      roomService.currentRoom = room;
+    };
+
 
     return roomService;
   }
