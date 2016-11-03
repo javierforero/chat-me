6e412bc662b5b024f4aef603950d6e24709710a9
Javier Forero <javierforero01@gmail.com>
2016-11-03 08:54:34 -0500

    autoscroll

--- app/scripts/directives/autoScroll.js
+++ app/scripts/directives/autoScroll.js
@@ -1,20 +1,20 @@
 (function() {
-  function autoScroll() {
-
-    return {
-      restrict: 'A',
-      scope: {},
-      link: function (scope, element) {
-        // scope.$watchCollection('autoScroll', function (newValue) {
-        //   if (newValue){
-        //     $(element).scrollTop($(element)[0].scrollHeight);
-        //   }
-        // });
-        alert('hi!');
-      }
+    function autoScroll() {
+      return {
+        scope: {
+          autoScroll: "="
+        },
+        link: function(scope, element) {
+          scope.$watchCollection('autoScroll', function(newValue){
+            if(newValue) {
+              $(element).scrollTop($(element)[0].scrollHeight);
+            }
+          });
+        }
+      };
     }
 
-  angular
-    .module('blocChat')
-    .directive('autoScroll', autoScroll);
+    angular
+        .module('blocChat')
+        .directive('autoScroll', autoScroll);
 })();
